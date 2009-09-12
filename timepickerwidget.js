aafdfec513ad89b375153c95a3200c4b715a76f8
Stephen Woods <saw@yahoo-inc.com>
2009-09-12 11:37:48 -0700

    rendering copied over

--- timepickerwidget.js
+++ timepickerwidget.js
@@ -4,12 +4,26 @@ YUI.add('timepicker', function(Y){
     Widget   = Y.Widget,
     Node     = Y.Node,
     NAMESPACE= 'Saw',
+    DISPLAY  = 'display',
+    NONE     = 'none',
     CONSTRUCTOR= 'Timepicker';
     
     /* utils */
     function pad(num){
         return (num < 10) ? '0' + num : num;
     };
+    
+    
+    function makeCell(str, rowId){
+        var nodeId = 'cn-' + str;
+       //rowLookup[nodeId] = rowId;
+        var str = '<li id="'+nodeId+'" class=" c-cell c-'+str+'"><span>'+str+'</span></li>';
+        return str;
+    }
+
+    function makeRow(str, c){
+        return '<ol id="'+c+'" class="c-row">'+str+'</ol>';
+    }
     
     function Timepicker(config) {
         
@@ -28,10 +42,7 @@ YUI.add('timepicker', function(Y){
             }
         }
     };
-    
 
-    
-    
     Y.extend(Timepicker, Widget, {
         initializer:function(){
             
@@ -42,7 +53,31 @@ YUI.add('timepicker', function(Y){
         },
         
         renderUI: function(){
-            
+
+            myNode = this.get('contentBox');
+            var rowStrings = [];
+            rowStrings.push(makeRow(makeCell('AM','prefix')+makeCell('PM','prefix'), 'c-prefix'));
+            var hour ='', minute='';
+            for(var i=1; i<=12; i++){
+                hour += makeCell(i , 'hour');
+            }
+            rowStrings.push(makeRow(hour, 'c-hour'));
+
+            for(var i=0; i<=45; i= i +15){
+                minute += makeCell(i, 'minute');
+            }
+
+            rowStrings.push(makeRow(minute, 'c-minute'));
+
+
+            //bd.appendChild(myNode);
+
+            myNode.set('innerHTML', rowStrings.join(''));
+            myNode.setXY([0,0]);
+            myNode.setStyle(DISPLAY, 'block');
+            // myNode.on('click', handleClicks);
+            // 
+            //            myNode.on('mouseover', handleOver);
         },
         
         bindUI: function(){
