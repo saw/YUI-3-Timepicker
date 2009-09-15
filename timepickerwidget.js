20ce9333916beba0e58408159bd6456d72e09c72
Stephen Woods <saw@yahoo-inc.com>
2009-09-15 14:30:44 -0700

    crazy refactoring

--- timepickerwidget.js
+++ timepickerwidget.js
@@ -1,29 +1,38 @@
 YUI.add('timepicker', function(Y){
     
-    var Lang = Y.Lang,
-    Widget   = Y.Widget,
-    Position = Y.WidgetPosition,
-    Node     = Y.Node,
-    NAMESPACE= 'Saw',
-    DISPLAY  = 'display',
-    NONE     = 'none',
-    CONSTRUCTOR= 'Timepicker';
+    var Lang    = Y.Lang,
+    Widget      = Y.Widget,
+    Position    = Y.WidgetPosition,
+    Node        = Y.Node,
+    getClassName= Y.ClassNameManager.getClassName,
+    NAMESPACE   = 'p',
+    DISPLAY     = 'display',
+    NONE        = 'none',
+    CONSTRUCTOR = 'Timepicker',
+    CELL_CLASS  = 'cell',
+    HOUR_CLASS  = 'hour',
+    MINUTE_CLASS= 'minute',
+    AMPM_CLASS  = 'ampm',
+    ROW         = 'row',
+    HOUR_ROW    = 'hourrow',
+    MINUTE_ROW  = 'minuterow',
+    AMPM_ROW    = 'ampmrow';
     
     /* utils */
     function pad(num){
         return (num < 10) ? '0' + num : num;
     };
     
+//<span class="'+Timepicker[CELL_CLASS]+' '+Timepicker[rowId]+'">'+str+'</span>
     
     function makeCell(str, rowId){
-        var nodeId = 'cn-' + str;
-       //rowLookup[nodeId] = rowId;
-        var str = '<li id="'+nodeId+'" class=" c-cell c-'+str+'"><span>'+str+'</span></li>';
+        var thisClass = Y.ClassNameManager.getClassName(Timepicker.NAME, Timepicker[str]);
+        var str = '<li class="'+thisClass+' '+Timepicker[CELL_CLASS]+' '+Timepicker[rowId]+'">'+str+'</li>';
         return str;
     }
 
     function makeRow(str, c){
-        return '<ol id="'+c+'" class="c-row">'+str+'</ol>';
+        return '<ol>'+str+'</ol>';
     }
     
     function Timepicker(config){
@@ -33,6 +42,14 @@ YUI.add('timepicker', function(Y){
     Timepicker.NAME = 'timepicker';
     
     Timepicker.ATTRS = {
+        
+        time:{
+            value:{
+                ampm:'am',
+                hour:0,
+                minute:0
+            }
+        },
     
         strings: {
             value: {
@@ -42,12 +59,19 @@ YUI.add('timepicker', function(Y){
         }
     };
     
-    Timepicker.HOUR_CLASS = Y.ClassNameManager.getClassName(Timepicker.NAME, "hour");
-    Timepicker.MINUTE_CLASS = Y.ClassNameManager.getClassName(Timepicker.NAME, "minute");
-    Timepicker.AMPM_CLASS = Y.ClassNameManager.getClassName(Timepicker.NAME, "ampm");
-    Timepicker.HIDDEN = Y.ClassNameManager.getClassName(Timepicker.NAME, "hidden");
+    Timepicker[HOUR_CLASS] = getClassName(Timepicker.NAME, HOUR_CLASS);
+    Timepicker[MINUTE_CLASS] = getClassName(Timepicker.NAME, MINUTE_CLASS);
+    Timepicker[AMPM_CLASS] = getClassName(Timepicker.NAME, AMPM_CLASS);
+    Timepicker[HOUR_CLASS] = getClassName(Timepicker.NAME, HOUR_CLASS, ROW);
+    Timepicker[MINUTE_CLASS] = getClassName(Timepicker.NAME, MINUTE_CLASS, ROW);
+    Timepicker[CELL_CLASS] = getClassName(Timepicker.NAME, CELL_CLASS);
+    
     
     Y.extend(Timepicker, Widget, {
+        
+              _model : {ampm:{},hour:{},minute:{}},
+        
+            
               initializer:function(){
                  
               },
@@ -56,59 +80,81 @@ YUI.add('timepicker', function(Y){
                   
               },
               
-              _handleOver:function(){
-                  console.log('over');
+              _handleClick:function(e){
+          
+                  if(e.target.test('.'+Timepicker[CELL_CLASS])){
+                      console.log('cell');
+                  }
+              },
+              
+              _handleOver:function(e){
+                  var targ = e.target;
+                  if(targ.test('.'+Timepicker[CELL_CLASS])){
+                      var time = {};
+                      var value = e.target.get('innerHTML');
+                      if(targ.hasClass(Timepicker[HOUR_CLASS])){
+                          this.set('time.hour',value);
+                      }else if (targ.hasClass(Timepicker[AMPM_CLASS])){
+                          this.set('time.ampm', value);
+                      }else{
+                          this.set('time.minute', value);
+                      }
+                      
+                  }
+                  this.syncUI();
               },
               
               renderUI: function(){
        
-                  var cb = this.get('contentBox');
-                 
-                  var rowStrings = [];
-                  rowStrings.push(makeRow(makeCell('AM','prefix')+makeCell('PM','prefix'), 'c-prefix'));
-                  var hour ='', minute='';
+                  var cb = this.get('contentBox'),
+                       m = this._model;
+                  this.set('xy',[20,20]);
+                  var row1 = cb.create('<ol>'),
+                      row2 = cb.create('<ol>'),
+                      row3 = cb.create('<ol>');
+                  
+                  var am = row1.create(makeCell('AM',AMPM_CLASS)),
+                      pm = row1.create(makeCell('PM',AMPM_CLASS));
+                  m[AMPM_CLASS]['AM'] = am;
+                  m[AMPM_CLASS]['PM'] = pm;
+                  row1.appendChild(am);
+                  row1.appendChild(pm);
                   for(var i=1; i<=12; i++){
-                      hour += makeCell(i , 'hour');
+                      var cell = row2.create(makeCell(i , HOUR_CLASS));
+                      m[HOUR_CLASS][i] = cell;
+                      row2.appendChild(cell);
                   }
-                  rowStrings.push(makeRow(hour, 'c-hour'));
-       
                   for(var i=0; i<=45; i= i +15){
-                      minute += makeCell(i, 'minute');
+                      var cell = row3.create(makeCell(i , MINUTE_CLASS));
+                      m[MINUTE_CLASS][i] = cell;
+                      row3.appendChild(cell);
                   }
        
-                  rowStrings.push(makeRow(minute, 'c-minute'));
-       
-                  cb.set('innerHTML', rowStrings.join(''));
+
+                  cb.appendChild(row1);
+                  cb.appendChild(row2);
+                  cb.appendChild(row3);
+                 // cb.set('innerHTML', rowStrings.join(''));
                   cb.setXY([0,0]);
                   cb.setStyle(DISPLAY, 'block');
-                  // cb.on('click', handleClicks);
-                  // 
-                  //                  .on('mouseover', handleOver);
+                  
+                 
               },
-              
-              // show: function(){
-              //     this.set('hidden', false);
-              //     this.syncUI();
-              // },
-              // 
-              // hide: function(){
-              //     
-              //     this.set('hidden', true);
-              //     this.syncUI();
-              // },
+
               
               bindUI: function(){
-                  var cb = this.get('contentBox')
-
-                  cb.on('mouseover', this._handleOver);
+                  var cb = this.get('contentBox');
+                  cb.on('click', this._handleClick, this);
+                  cb.on('mouseover', this._handleOver, this);
               },
               
               syncUI: function(){
-                  if(this.get('hidden')){
-                      this.get('contentBox').addClass(Timepicker.HIDDEN);
-                  }else{
-                      this.get('contentBox').removeClass(Timepicker.HIDDEN);
-                  }
+                  var time = this.get('time');
+                  cells = this.get('contentBox').queryAll('li');
+                  cells.each(function(cell){
+                      cell.removeClass('active');
+                  });
+                  
               }
           });
           
